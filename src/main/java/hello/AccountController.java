492c3d0224a8de511bfe359f030457be32c248ee
Nick Mattei <nmattei89@gmail.com>
2015-10-06 16:08:16 -0600

    path variables working

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -3,14 +3,26 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.mongodb.MongoClient;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 
 @RestController
@@ -19,8 +31,13 @@ public class AccountController {
 	@Autowired
 	public PersonRepository repository;
 
-    @RequestMapping("/account")
-    public String accountName() throws IOException{
+//	MongoClient mongoClient = new MongoClient( "localhost/grockr" );
+
+
+
+    @RequestMapping("/account/{id1}/{id2}")
+    public String accountName(@PathVariable("id1") int id, @PathVariable("id2") int id1) throws IOException{
+    	System.out.println(id+id1);
     	Stock stock = YahooFinance.get("INTC");
     	BigDecimal price = stock.getQuote(true).getPrice();
     	System.out.println("The current price of INTEL is:" +price);
--- src/main/java/hello/PersonRepository.java
+++ src/main/java/hello/PersonRepository.java
@@ -1,7 +1,6 @@
 package hello;
 
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
