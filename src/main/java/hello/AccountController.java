8334ecfbc1cc1de643dd4231682a02a22b8575aa
Nick Mattei <nmattei89@gmail.com>
2015-10-06 15:03:18 -0600

    yahoo finance up

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -1,10 +1,14 @@
 package hello;
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.reflect.Array;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -16,9 +20,11 @@ public class AccountController {
 	public PersonRepository repository;
 
     @RequestMapping("/account")
-    public String accountName(){
+    public String accountName() throws IOException{
+    	Stock stock = YahooFinance.get("INTC");
+    	BigDecimal price = stock.getQuote(true).getPrice();
+    	System.out.println("The current price of INTEL is:" +price);
     	repository.save(new Person("WORK PLEASE","Longbottom"));
-
     	return "BRAND NEW NEW ACCOUNT";
     }
     
