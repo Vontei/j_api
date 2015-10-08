01ce802b19a0b28d8f3769bb7a5ad631e4e9f700
Nick Mattei <nmattei89@gmail.com>
2015-10-07 20:10:31 -0600

    WINNING

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -1,11 +1,20 @@
 package hello;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.mongodb.MongoClient;
 import yahoofinance.Stock;
@@ -13,16 +22,16 @@ import yahoofinance.YahooFinance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.BulkWriteOperation;
-import com.mongodb.BulkWriteResult;
-import com.mongodb.Cursor;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
+//
+//import com.mongodb.BasicDBObject;
+//import com.mongodb.BulkWriteOperation;
+//import com.mongodb.BulkWriteResult;
+//import com.mongodb.Cursor;
+//import com.mongodb.DB;
+//import com.mongodb.DBCollection;
+//import com.mongodb.DBCursor;
+//import com.mongodb.DBObject;
+//import com.mongodb.MongoClient;
 
 
 @RestController
@@ -36,19 +45,39 @@ public class AccountController {
 
 
     @RequestMapping("/account/{id1}/{id2}")
-    public String accountName(@PathVariable("id1") int id, @PathVariable("id2") int id1) throws IOException{
+    public String[] accountName(@PathVariable("id1") int id, @PathVariable("id2") int id1) throws IOException{
     	System.out.println(id+id1);
-    	Stock stock = YahooFinance.get("INTC");
+    	int sum = id+id1;
+    	Stock stock = YahooFinance.get("GOOG");
     	BigDecimal price = stock.getQuote(true).getPrice();
-    	System.out.println("The current price of INTEL is:" +price);
+    	System.out.println("The current price of GOOGLE is:" +price);
     	repository.save(new Person("WORK PLEASE","Longbottom"));
-    	return "BRAND NEW NEW ACCOUNT";
+    	String[] results = {price.toString(), Integer.toString(sum)}; 
+    	return results;
     }
     
     
+	@RequestMapping(value="/account/trial" , method=RequestMethod.POST)
+    public String doStuff(HttpServletRequest request, HttpServletResponse response) throws IOException{
+			  StringBuffer jb = new StringBuffer();
+			  String line = null;
+			  try {
+			    BufferedReader reader = request.getReader();
+			    while ((line = reader.readLine()) != null)
+			      jb.append(line);
+			  } catch (Exception e) { /*report an error*/ }
+
+			 System.out.println(jb);
+			 System.out.println(jb.toString());
+    	return null;
+      }
+    	
+    
+  
+    
     @RequestMapping("/account/people")   
     public List<String> accountPeople(){
-		List<String> x = new ArrayList<String>();
+		List<String> x = new ArrayList<String>();	
     	for (Person person : repository.findAll()) {
     		x.add(person.toString());
 		}
