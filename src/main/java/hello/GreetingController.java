33b4f82d6d0f35c14e0fb309a5ace671dbead58e
Nick Mattei <nmattei89@gmail.com>
2015-10-08 18:46:39 -0600

    quick file clean-up

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class GreetingController {
 	public PersonRepository repository;
@@ -13,12 +14,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-//    @RequestMapping(value="/greeting", method=RequestMethod.POST)
-//    public void newGreeting(@RequestParam(value="name", defaultValue="World") String name) {
-//    	repository.save(new Person(name, "BANANAS"));
-//    	
-//    }
-    
+
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     	return new Greeting(counter.incrementAndGet(),
@@ -35,5 +31,12 @@ public class GreetingController {
     	return "HELLO THERE";
     }
     
+    
+//    @RequestMapping(value="/login" , method=RequestMethod.POST)
+//    public login(){
+//    	UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+//
+//    }	
+    		
     
 }
--- src/main/java/hello/SpringMongoConfig1.java
+++ src/main/java/hello/SpringMongoConfig1.java
@@ -1,48 +1,12 @@
 package hello;
 
-//
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.MongoDbFactory;
-//import org.springframework.data.mongodb.core.MongoOperations;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-//
-//import com.mongodb.MongoClient;
-//
-//@Configuration
-//public class SpringMongoConfig1 {
-//
-//	public @Bean
-//	MongoDbFactory mongoDbFactory() throws Exception {
-//		return new SimpleMongoDbFactory(new MongoClient(), "grockr");
-//	}
-//
-//	public @Bean
-//	MongoTemplate mongoTemplate() throws Exception {
-//		
-//		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-//				
-//		return mongoTemplate;
-//		
-//	}
-//	ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig1.class);
-//    MongoOperations mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");
-//
-//}
-
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import com.mongodb.MongoClient;
 
-/**
- * Spring MongoDB configuration file
- * 
- */
+
 @Configuration
 public class SpringMongoConfig1{
 
